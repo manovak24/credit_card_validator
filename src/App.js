320f149dea66f18be62715a530147d710f2449c8
emagdnimMQT <mark.w.novak@gmail.com>
2021-04-15 12:47:54 -0400

    Input field works at updating the card number state in app js.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
+import CardValidator from './Components/card-validator';
 
 
 //Dont forget to use the Google fonts poppins and work sans
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardNumber: ''
+    }
+
+    this.updateCardNumber = this.updateCardNumber.bind(this);
+  }
+
+  updateCardNumber(card) {
+    this.setState({ cardNumber: card });
+  }
+  
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <input type="text"></input>
+        <CardValidator onNumberChange={this.updateCardNumber} />
       </div>
       
     )
--- src/Components/card-validator.js
+++ src/Components/card-validator.js
@@ -3,22 +3,23 @@ import React from 'react';
 class CardValidator extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            cardNumber: ''
-        }
-
+        this.handleChange = this.handleChange.bind(this);
     }
     
+    handleChange(event) {
+        this.props.onNumberChange(event.target.value);
+    }
+
     render() {
         return(
             <div className="card-validator">
                 <input
+                onChange={this.handleChange}
                 onKeyPress={event => {
                     if (event.key === "Enter") {
                         this.search();
                     }
                 }}
-                //onChange={this.handleTermChange}
                 placeholder="Please enter card number" />
                 <button onClick={this.search} className="validate-button">Validate</button>
             </div>
