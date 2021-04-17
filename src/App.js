9fffd1bd7e146a06076a37c24088946cbdd85baa
emagdnimMQT <mark.w.novak@gmail.com>
2021-04-17 17:16:49 -0400

    Credit card validator logic works.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import './App.css';
 import CardInfo from './Components/card-info';
-import ValidateCred from './Util/card-validator';
+
 
 //Dont forget to use the Google fonts poppins and work sans
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      results: ''
-    }
-
-    this.searchCard = this.searchCard.bind(this);
-  }
-
-  searchCard(cardNumber) {
-    ValidateCred.validate(cardNumber).then(results => {
-      this.setState({results: results})
-    })
-  }
-
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <CardInfo searchCard={this.searchCard} />
-        <p>You card is {this.state.results}</p>
+        <CardInfo />
       </div>
       
     )
--- src/Components/card-info.js
+++ src/Components/card-info.js
@@ -16,9 +16,23 @@ class CardInfo extends React.Component {
         this.setState({ cardNumber: event.target.value })
     }
 
-    handleSearch(card) {
-        card = this.state.cardNumber;
-        if (card === '4539677908016808'){
+    handleSearch() {
+        let card = this.state.cardNumber;
+        card = card.split('').join(', ');
+        
+        const cardToArr = card.split(',').map(function(item) {
+            return parseInt(item, 10);
+        })
+
+        cardToArr.slice();
+        for (let i = (cardToArr.length - 2); i >= 0; i = i -2) {
+            cardToArr[i] = cardToArr[i] * 2;
+            if (cardToArr[i] > 9) {
+                cardToArr[i] = cardToArr[i] -9;
+            }
+        }
+        let arraySum = cardToArr.reduce((acc, cur) => acc + cur);
+        if (arraySum % 10 === 0) {
             this.setState({results: 'Working'})
         } else {
             this.setState({results: 'Not Working'})
