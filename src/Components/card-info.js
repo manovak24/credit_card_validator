c8c4e58f6e9e8bfa411da2946cfd7e559936ad98
emagdnimMQT <mark.w.novak@gmail.com>
2021-04-17 11:11:15 -0400

    Created card validator component to run validation.

---
+++ src/Components/card-info.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class CardInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleChange(event) {
+        this.props.onNumberChange(event.target.value);
+    }
+
+    render() {
+        return(
+            <div className="card-validator">
+                <input
+                onChange={this.handleChange}
+                onKeyPress={event => {
+                    if (event.key === "Enter") {
+                        this.search();
+                    }
+                }}
+                placeholder="Please enter card number" />
+                <button onClick={this.search} className="validate-button">Validate</button>
+            </div>
+        )
+    }
+}
+
+export default CardInfo;
