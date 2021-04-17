3a8e665a1c79beccd42247634f5d4c3982b9dc83
emagdnimMQT <mark.w.novak@gmail.com>
2021-04-17 16:55:07 -0400

    Back on track. handleSearch needs code to do validation.

--- src/Components/card-info.js
+++ src/Components/card-info.js
@@ -16,21 +16,13 @@ class CardInfo extends React.Component {
         this.setState({ cardNumber: event.target.value })
     }
 
-    handleSearch(cardNumber) {
-        let array = cardNumber; 
-        for (let i = (array.length - 2); i >= 0; i = i - 2) {
-        //console.log(array);
-        array[i] = array[i] * 2; 
-        if (array[i] > 9) {
-            array[i] = array[i] - 9;
-        } 
-    }
-        let arraySum = array.reduce((acc, cur) => acc + cur); 
-        if (arraySum % 10 === 0) {
-            this.setState({ results: 'Working' })  
+    handleSearch(card) {
+        card = this.state.cardNumber;
+        if (card === '4539677908016808'){
+            this.setState({results: 'Working'})
         } else {
-            this.setState({ results: 'Not Working' })
-        }   
+            this.setState({results: 'Not Working'})
+        }
     }  
 
     render() {
