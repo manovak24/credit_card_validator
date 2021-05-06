2cef189c9f0619caf2ed192493b866b0c22b7053
emagdnimMQT <mark.w.novak@gmail.com>
2021-05-06 12:59:18 -0400

    added footer to app js and included font awesome icons for linked and github.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import './App.css';
 import CardInfo from './Components/card-info';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 //Dont forget to use the Google fonts poppins and work sans
 class App extends React.Component {
   render() {
     return (
-      <div className="app-container">
-        <div className="title-container">
-          <h1>Credit Card Validator</h1>
+      <div className="body-container">
+
+        <div className="app-container">
+          <div className="title-container">
+            <h1>Credit Card Validator</h1>
+          </div>
+
+          <div className="card-info-container">
+            <CardInfo />
+          </div>
+
+          <div className="app-footer-container">
+            <p>*Please note this site does not store any credit card information*</p>
+          </div>
         </div>
 
-        <div className="card-info-container">
-          <CardInfo />
+        <div className="footer">
+          <a href="https://www.linkedin.com/in/mark-novak-56679949/" className="linkedin social" rel="noopener noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+          <a href="https://manovak24.github.io/" rel="noopener noreferrer" target="_blank">
+            <p>Mark Novak</p>
+          </a>
+          
+          <a href="https://github.com/manovak24" className="github social" rel="noopener noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          
         </div>
 
-        <div className="footer-container">
-          <p>*Please note this site does not store any credit card information*</p>
-        </div>
       </div>
       
     )
